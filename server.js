c7178f10d82eb15b1b84a33c25be5c4f5ecbefe6
ghuppert <grahamhuppert@hotmail.com>
2015-07-15 23:39:53 -0700

    testing changes to azure
    testing

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ var http = require('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World of future game hub users and bannanaws \n");
+  response.send("Hello World of future game hub users and bannanaws \n");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
